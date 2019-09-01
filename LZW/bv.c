47fc1ac11be5366b55da0cb839fda61ac0a2dffa
Robert Sato <rssato@ucsc.edu>
2019-09-01 11:10:40 -0700

    finished bv.c but 1 confusing memory leak

--- LZW/bv.c
+++ LZW/bv.c
@@ -31,11 +31,60 @@ void bv_delete(BitVector *v) {
 void bv_set_bit(BitVector *v, uint8_t i) {
    uint8_t index = i%8;
    uint8_t byte_index = i/8;
-   v->vector[byte_index] |= 1UL << index;
+   v->vector[byte_index] |= 1U << index;
 }
 
 void bv_clr_bit(BitVector *v, uint8_t i) {
    uint8_t index = i%8;
    uint8_t byte_index = i/8;
-   v->vector[byte_index] &= ~(1UL << index);
+   v->vector[byte_index] &= ~(1U << index);
+}
+
+uint8_t bv_get_bit(BitVector *v, uint8_t i) {
+   uint8_t index = i%8;
+   uint8_t byte_index = i/8;
+   uint8_t bit = (v->vector[byte_index] >> index) & 1U;
+   return bit;
+   
+}
+
+BitVector *code_num_to_bv(uint64_t code_num, uint8_t bit_len) {
+   BitVector *bv = bv_create(bit_len);
+   uint8_t bit;
+   //printf("code_num is %lu with length %u\n", code_num, bit_len);
+   for(int i = 0; i < bit_len; i++) {
+      bit = (code_num >> i) & 1UL;
+      if(bit) { bv_set_bit(bv, i); }
+   }
+   printf("\n");
+
+   return bv;
+}
+
+uint64_t bv_to_code_num(BitVector *v) {
+   int i;
+   uint8_t bit, byte_index, index;
+   uint64_t code = 0;
+   //printf("code_num is %lu with length %u\n", code_num, bit_len);
+   for(i = v->length - 1; i >= 0; i--) {
+      //printf("code: %lu\n", code);
+      code = code << 1;
+      byte_index = i/8;
+      index = i%8;
+      bit = (v->vector[byte_index] >> index) & 1UL;
+      if(bit) { code++; }
+   }
+   return code;
+}
+
+void bv_print(BitVector *v) {
+   int i;
+   uint8_t bit, byte_index, index;
+   //printf("code_num is %lu with length %u\n", code_num, bit_len);
+   for(i = 0; i < v->length; i++) {
+      byte_index = i/8;
+      index = i%8;
+      bit = (v->vector[byte_index] >> index) & 1UL;
+      printf("%u", bit);
+   }
 }
--- LZW/main.c
+++ LZW/main.c
@@ -8,21 +8,45 @@
 
 int main() {
    printf("This is the main function of LZW\n");
-   char *word = "temp";
+
+   //ListNode testing
+/*   char *word = "temp";
    ListNode *listNode = ll_node_create(98, (uint8_t *)word, 4);
    printf("Printing the contents of listNode\n");
    ll_node_print(listNode);
    ll_node_delete(listNode);
-   //done with listnode
-   printf("Creating BitVector\n");
+*/
+   //BitVector testing
+/*   printf("Creating BitVector\n");
    BitVector *bv = bv_create(10);
    uint64_t byte_length = byte_len(10);
    printf("bv's length = %ld\n", byte_length);
+   printf("getbit 9\n");
+   printf("%u\n",  bv_get_bit(bv, 9) );
+   bv_set_bit(bv, 9);
+   printf("getbit 9 after setting\n");
+   printf("%u\n",  bv_get_bit(bv, 9) );
+   bv_clr_bit(bv, 9);
+   printf("getbit 9 after clearing\n");
+   printf("%u\n",  bv_get_bit(bv, 9) );
    printf("Deleting the bv\n");
    bv_delete(bv);
-   //done with bv
-   printf("\nTesting bit operations\n");
+*/
+   BitVector *codebv = code_num_to_bv(5, 9);
+   printf("contents of codebv(printed from LSB in bv->vector): ");
+   bv_print(codebv);
+   printf("\n");
+   BitVector *temp = codebv;
+   uint64_t code;
+   code = bv_to_code_num(codebv);
+   printf("code = %lu\n", code);
+   bv_delete(temp);
+
+   //bit operation testing
+/*   printf("\nTesting bit operations\n");
    printf("1UL = %lu\n", 1UL);
    printf("1UL << 1 = %lu\n", (1UL << 1));
+   printf("1U = %u\n", 1U);
+*/
    return 0;
 }
